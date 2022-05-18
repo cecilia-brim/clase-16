415711a48f2fe188e67a9b8a589b26405d444914
cecilia brim <cecibrim@gmail.com>
2022-05-18 18:54:04 -0300

    agrego el main a mi repo

---
+++ main.py
@@ -0,0 +1,8 @@
+#punto de entrada para lo que voy a programar
+
+import paquete_maths.modulo_maths as modulo_maths
+
+
+#o from paquete_maths.modulo_maths import sumar
+print(modulo_maths.sumar(1,3))
+
