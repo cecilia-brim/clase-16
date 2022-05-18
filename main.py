3a4a2df9baf74092ed757de41cb51d0e7e597d45
cecilia brim <cecibrim@gmail.com>
2022-05-18 19:08:29 -0300

    primer cambio

--- main.py
+++ main.py
@@ -6,3 +6,18 @@ import paquete_maths.modulo_maths as modulo_maths
 #o from paquete_maths.modulo_maths import sumar
 print(modulo_maths.sumar(1,3))
 
+
+#para inicializar repositorio se pone git init
+#para ver ocultos de git se pone ls -a 
+#para ver todos se pone ls
+#git status me muestra cual es el estado de mi repositorio
+#para agregar archivos a mi rep se pone git add y el nombre
+#para guardar es commit se le pasa un mensaje de lo que hice
+#para agregar carpetas git add y ademas del nombre una barra al final
+#luego de agragarlos estan como en el limbo en el staging area
+#luego con el commit se guarda y pasa a repositorio
+#git log me muestra lo que commit  mi repositorio
+#rama es para hacer algo en paralelo que no esta seguro que este bien etc
+#git graph
+
+print("buenas")
