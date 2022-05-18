7c1865895a3a2cb37891b49582b23a4944b566f7
cecilia brim <cecibrim@gmail.com>
2022-05-18 18:57:41 -0300

    agregue el paquete maths

---
+++ paquete_maths/modulo_maths.py
@@ -0,0 +1,24 @@
+def sumar(a,b):
+    return a+b
+
+
+def restar(a,b):
+    return a-b
+
+
+class Conjunto:
+    pass
+
+
+#Mínimo y maximo de listas
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(f"Ejemplo mínimo: {x}")
+print(f"Ejemplo máximo: {y}")
+
+
+
+#Valor absoluto, módulo
+x = -11.3
+print(f"Ejemplo valor absoluto : {x}")
